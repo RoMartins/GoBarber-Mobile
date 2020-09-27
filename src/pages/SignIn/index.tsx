c9248e1f06c5c40ed7cf17f607ed532772d63c88
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-27 01:43:15 -0300

    Create pages SignIn andSignUp

---
+++ src/pages/SignIn/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignIn: React.FC = () => {
+  return <Container />;
+};
+
+export default SignIn;
