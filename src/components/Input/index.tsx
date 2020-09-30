7093eac3b1d6d285b5096ca6aaf8fb8e9d06ba76
RoMartins <rodrigomartins_santos1804@hotmail.com>
2020-09-30 00:31:17 -0300

    RegisterFields with unform

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { TextInputProps } from 'react-native';
+import { useField } from '@unform/core';
 import { Container, TextInput, Icon } from './styles';
 
 interface InputProps extends TextInputProps {
   name: string;
   icon: string;
 }
-const Input: React.FC<InputProps> = ({ icon, name, ...rest }) => (
-  <Container>
-    <Icon name={icon} size={20} color="#666360" />
-    <TextInput
-      placeholderTextColor="#666360"
-      {...rest}
-      keyboardAppearance="dark"
-    />
-  </Container>
-);
+interface inputValue {
+  value: string;
+}
+const Input: React.FC<InputProps> = ({ icon, name, ...rest }) => {
+  const { fieldName, registerField, defaultValue = '' } = useField(name);
+  const inputValueRef = useRef<inputValue>({ value: defaultValue });
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputValueRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+  return (
+    <Container>
+      <Icon name={icon} size={20} color="#666360" />
+      <TextInput
+        placeholderTextColor="#666360"
+        {...rest}
+        defaultValue={defaultValue}
+        keyboardAppearance="dark"
+        onChangeText={value => {
+          inputValueRef.current.value = value;
+        }}
+      />
+    </Container>
+  );
+};
 
 export default Input;
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useRef } from 'react';
 import { useNavigation } from '@react-navigation/native';
 import {
   Image,
@@ -8,6 +8,8 @@ import {
   View,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
+import { Form } from '@unform/mobile';
+import { FormHandles } from '@unform/core';
 import logoImg from '../../assets/Logo.png';
 import Button from '../../components/Button';
 import Input from '../../components/Input';
@@ -23,6 +25,11 @@ import {
 
 const SignIn: React.FC = () => {
   const navigation = useNavigation();
+  const FormRef = useRef<FormHandles>(null);
+  const handleSignIn = useCallback((data: object) => {
+    console.log(data);
+  }, []);
+
   return (
     <>
       <KeyboardAvoidingView
@@ -41,16 +48,22 @@ const SignIn: React.FC = () => {
               <Title>Faça seu logon</Title>
             </View>
 
-            <Input name="email" icon="mail" placeholder="E-mail" />
-            <Input name="password" icon="lock" placeholder="Senha" />
-
-            <Button
-              onPress={() => {
-                console.log('oláa');
-              }}
+            <Form
+              ref={FormRef}
+              style={{ width: '100%' }}
+              onSubmit={handleSignIn}
             >
-              Entrar
-            </Button>
+              <Input name="email" icon="mail" placeholder="E-mail" />
+              <Input name="password" icon="lock" placeholder="Senha" />
+
+              <Button
+                onPress={() => {
+                  FormRef.current?.submitForm();
+                }}
+              >
+                Entrar
+              </Button>
+            </Form>
 
             <ForgotPassword
               onPress={() => {
